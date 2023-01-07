b3d867c7cbd1555e219df58fb0f55d1150f48ad0
Evan Andrian <efan.andrian05@gmail.com>
2023-01-07 14:02:19 +0700

    first commit

---
+++ DS_D.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+"D:\ds_rsptpnsubung\kill_DS.exe" $1 &
+git pull
+"D:\ds_rsptpnsubung\DesktopService.exe" $1 &
+
---
+++ DS_E.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+"E:\ds_rsptpnsubung\kill_DS.exe" $1 &
+git pull
+"E:\ds_rsptpnsubung\DesktopService.exe" $1 &
+
+
